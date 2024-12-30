6f90ba59b5041ed37fd9ba044d5cc9e9ade0661b
JL-GALE <jorge.galem@gmail.com>
2024-12-30 14:40:10 -0500

    Se crearon las clases controller y RESTController, se pasaron datos a la vista utilizando thymeleaf, se creo el modelo user y se creo el userDTO, se utilizaron anotaciones como @RequesMapping y @GetMapping

--- springbootWeb/src/main/java/com/jorge/springboot/webapp/controllers/UserRestController.java
+++ springbootWeb/src/main/java/com/jorge/springboot/webapp/controllers/UserRestController.java
@@ -1,20 +1,31 @@
 package com.jorge.springboot.webapp.controllers;
 
 import com.jorge.springboot.webapp.model.User;
+import com.jorge.springboot.webapp.model.dto.UserDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 
-    @GetMapping("/details1")
+    @GetMapping("/details-dto")
+    public UserDto detailsDto() {
+        User user = new User("JorgeDTO", "Gale");
+        UserDto userDto = new UserDto();
+        userDto.setUser(user);
+        userDto.setTitle("Hola world desde un dto kkkk");
+        return userDto;
+    }
+
+    @GetMapping("/details-map")
     public Map details() {
-        User user = new User("Milton","Olivero");
+        User user = new User("Milton-map", "Olivero");
         Map<String, Object> body = new HashMap<>();
         body.put("User", user);
         body.put("title", "Hello world");
---
+++ springbootWeb/src/main/java/com/jorge/springboot/webapp/model/dto/UserDto.java
@@ -0,0 +1,24 @@
+package com.jorge.springboot.webapp.model.dto;
+
+import com.jorge.springboot.webapp.model.User;
+
+public class UserDto {
+    private String title;
+    private User user;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
