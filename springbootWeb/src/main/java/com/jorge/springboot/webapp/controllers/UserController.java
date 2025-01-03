3dadb196ce7b907a595e16068263903991f6c4e3
JL-Gale <jorge.galem@gmail.com>
2025-01-02 21:51:17 -0500

    Se parctico con cargar una lista a la vista de thymeleaf y se utilizo a directiva de each para mostrarla

--- springbootWeb/src/main/java/com/jorge/springboot/webapp/controllers/UserController.java
+++ springbootWeb/src/main/java/com/jorge/springboot/webapp/controllers/UserController.java
@@ -3,7 +3,11 @@ package com.jorge.springboot.webapp.controllers;
 import com.jorge.springboot.webapp.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -15,5 +19,17 @@ public class UserController {
         model.addAttribute("title", "Hello world");
         model.addAttribute("user", user);
         return "details";
+    }
+
+    @GetMapping("/list")
+    public String lista(ModelMap model){
+        List<User> list = Arrays.asList(
+                new User("Luisa", "Montiel"),
+                new User("Nidia", "Orozco", "Nidia@gmail.com"),
+                new User("Jorge", "Gale", "jorge.@gmail.com"));
+        model.addAttribute("title", "Lista de atributos");
+        model.addAttribute("textoh1", "Lista de usarioss");
+        model.addAttribute("users", list);
+        return "list";
     }
 }
