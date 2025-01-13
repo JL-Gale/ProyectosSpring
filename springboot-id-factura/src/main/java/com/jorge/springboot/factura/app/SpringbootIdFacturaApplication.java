2bc12bb3db805fa49bc3f400dad2c63893ca2d5c
JL-Gale <jorge.galem@gmail.com>
2025-01-13 17:35:22 -0500

    Creacion de nuevo proyecto para seguir estudiando la inyeccion de dependencias, el proyecto tiene Spring Web, Spring Boot DevTools y Spring Boot Actuator

---
+++ springboot-id-factura/src/main/java/com/jorge/springboot/factura/app/SpringbootIdFacturaApplication.java
@@ -0,0 +1,13 @@
+package com.jorge.springboot.factura.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIdFacturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIdFacturaApplication.class, args);
+	}
+
+}
