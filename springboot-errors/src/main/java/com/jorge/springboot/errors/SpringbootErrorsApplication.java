4a9308cd2dc2a346b3e3b29f90f578a626c61e84
JL-Gale <jorge.galem@gmail.com>
2025-02-06 13:29:08 -0500

    Se creo nuevo proyecto para practicas el manejos de los errores en springboot

---
+++ springboot-errors/src/main/java/com/jorge/springboot/errors/SpringbootErrorsApplication.java
@@ -0,0 +1,13 @@
+package com.jorge.springboot.errors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootErrorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootErrorsApplication.class, args);
+	}
+
+}
